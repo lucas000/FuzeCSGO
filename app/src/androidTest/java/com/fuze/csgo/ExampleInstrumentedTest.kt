2e56e4c599b3dd52d16d815f90711265b24294cc
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2022-11-02 20:49:56 -0300

    create base to start instrumenteds tests and unit tests

--- app/src/androidTest/java/com/fuze/csgo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fuze/csgo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.fuze.csgo
+package com.lucas.testesicredi
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fuze.csgo", appContext.packageName)
+        assertEquals("com.lucas.testesicredi", appContext.packageName)
     }
 }
